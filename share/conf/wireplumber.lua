cfcabd2c3ac6481eafb237912097ad2528297479
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2026-03-16 18:52:40 +1000

    Fixed minor ALSA audio buffering issue

--- share/conf/wireplumber.lua
+++ share/conf/wireplumber.lua
@@ -7,10 +7,14 @@ rules = {
     },
     apply_properties = {
       ["node.pause-on-idle"] = false,
+      ["node.always-process"] = true,
       ["session.suspend-timeout-seconds"] = 0,
-      ["api.alsa.period-size"] = 192,
+      ["priority.driver"] = 2000,
+      ["priority.session"] = 2000,
+      ["api.alsa.period-size"] = 256,
       ["api.alsa.periods"] = 3,
-      ["api.alsa.headroom"] = 0,
+      ["api.alsa.headroom"] = 128,
+      ["api.alsa.disable-batch"] = true,
     },
   },
 }
