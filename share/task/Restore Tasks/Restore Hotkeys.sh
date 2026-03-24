44a17d0e94bfe3d5b01b0c7953999c2cfc458293
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2026-03-24 22:26:54 +1000

    Updated hotkey runners to use MENU

--- share/task/Restore Tasks/Restore Hotkeys.sh
+++ share/task/Restore Tasks/Restore Hotkeys.sh
@@ -16,20 +16,20 @@ TUI_INI="$MUOS_SHARE_DIR/hotkey/tui.ini"
 cat > "$RG_INI" << 'EOF'
 0=["R2","L2","A"]
 1=["R2","L2","X"]
-2=["L1","MENU_LONG","A"]
-3=["L1","MENU_LONG","X"]
-4=["MENU_LONG","START"]
-5=["MENU_LONG","SELECT"]
+2=["L1","MENU","A"]
+3=["L1","MENU","X"]
+4=["MENU","START"]
+5=["MENU","SELECT"]
 EOF
 
 # Replace tui.ini with defaults
 cat > "$TUI_INI" << 'EOF'
 0=["R2","L2","A"]
 1=["R2","L2","X"]
-2=["L1","MENU_SHORT","A"]
-3=["L1","MENU_SHORT","X"]
-4=["MENU_SHORT","START"]
-5=["MENU_SHORT","SELECT"]
+2=["L1","MENU","A"]
+3=["L1","MENU","X"]
+4=["MENU","START"]
+5=["MENU","SELECT"]
 EOF
 
 echo "Sync Filesystem"
