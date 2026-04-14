e4541a86692ecafcf53c9d264515055eb34ec6ff
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2026-04-14 21:07:16 +1000

    Added Mustard Terminal application

---
+++ share/application/Mustard Terminal/mux_launch.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# HELP: A virtual terminal for direct shell access, be careful out there!
+# ICON: terminal
+# GRID: Terminal
+
+. /opt/muos/script/var/func.sh
+
+APP_BIN="muterm"
+SETUP_APP "$APP_BIN" ""
+
+# -----------------------------------------------------------------------------
+
+cd "$HOME" || exit
+/opt/muos/frontend/muterm
