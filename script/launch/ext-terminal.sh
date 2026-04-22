813a94b4debd7fd6578101ca7751081f46f2f1dd
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2026-04-22 20:18:34 +1000

    Added additional external emulator launch for Mustard Terminal

---
+++ script/launch/ext-terminal.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+. /opt/muos/script/var/func.sh
+
+NAME=$1
+CORE=$2
+FILE=${3%/}
+
+LOG_INFO "$0" 0 "Content Launch" "DETAIL"
+LOG_INFO "$0" 0 "NAME" "$NAME"
+LOG_INFO "$0" 0 "CORE" "$CORE"
+LOG_INFO "$0" 0 "FILE" "$FILE"
+
+HOME="$(GET_VAR "device" "board/home")"
+export HOME
+
+SETUP_STAGE_OVERLAY
+SETUP_SDL_ENVIRONMENT
+
+SET_VAR "system" "foreground_process" "muterm"
+
+/opt/muos/frontend/muterm --gl -- "$FILE"
+
